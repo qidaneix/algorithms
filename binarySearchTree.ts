a2ec76a46b7a2c647f899b55c8a9d07e3482fdd6
jinxiao <jinxiao@tencent.com>
2021-08-24 15:47:30 +0800

    anyong

--- binarySearchTree.ts
+++ binarySearchTree.ts
@@ -1,5 +1,5 @@
 interface binarySearchTree {
-  data?: number;
+  data: number;
   left?: binarySearchTree;
   right?: binarySearchTree;
 }
@@ -34,11 +34,11 @@ const obj: binarySearchTree = {
 
 class BinarySearchTree {
   data: binarySearchTree;
-  constructor(obj) {
+  constructor(obj: binarySearchTree) {
     this.data = obj;
   }
   find(x: number) {
-    function f(bst: binarySearchTree) {
+    function f(bst: binarySearchTree | undefined): any {
       if (!bst) return null;
       if (x === bst.data) return bst;
       if (x < bst.data) return f(bst.left);
@@ -47,7 +47,7 @@ class BinarySearchTree {
     return f(this.data);
   }
   findWithoutRe(x: number) {
-    let node = this.data;
+    let node: binarySearchTree | undefined = this.data;
     while (node) {
       if (node.data === x) {
         return node;
@@ -61,9 +61,9 @@ class BinarySearchTree {
     return null;
   }
   findMin() {
-    function f(bst: binarySearchTree) {
-      if (bst.left) f(bst.left);
-      return bst.data;
+    function f(bst: binarySearchTree): any {
+      if (!bst.left) return bst.data;
+      return f(bst.left);
     }
     return f(this.data);
   }
@@ -73,9 +73,9 @@ class BinarySearchTree {
     return node.data;
   }
   findMax() {
-    function f(bst: binarySearchTree) {
-      if (bst.right) f(bst.right);
-      return bst.data;
+    function f(bst: binarySearchTree): any {
+      if (!bst.right) return bst.data;
+      return f(bst.right);
     }
     return f(this.data);
   }
@@ -88,13 +88,13 @@ class BinarySearchTree {
     function f(bst: binarySearchTree) {
       if (x > bst.data) {
         if (!bst.right) {
-          bst.right.data = x;
+          bst.right = { data: x };
           return;
         }
         f(bst.right);
-      } else if (x < bst.left) {
+      } else if (x < bst.data) {
         if (!bst.left) {
-          bst.left.data = x;
+          bst.left = { data: x };
           return;
         }
         f(bst.left);
@@ -103,5 +103,50 @@ class BinarySearchTree {
     f(this.data);
     return this.data;
   }
-  delete(x: number) {}
+  delete(x: number) {
+    let node: binarySearchTree | undefined = this.data;
+    let deleted: binarySearchTree | undefined;
+    let deletedPapa: binarySearchTree | undefined = undefined;
+    while (node) {
+      if (node.data === x) {
+        deleted = node;
+        break;
+      }
+      if (node.data > x) {
+        deletedPapa = node;
+        node = node.left;
+      } else if (node.data < x) {
+        deletedPapa = node;
+        node = node.right;
+      }
+    }
+    if (!deleted) {
+      throw new Error("do not");
+    }
+    // 被删节点最多一个子树
+    if (!deleted.left || !deleted.right) {
+      if (!deletedPapa) {
+        this.data =
+          (deleted.left as binarySearchTree) ||
+          (deleted.right as binarySearchTree);
+      } else if (deletedPapa.left === deleted) {
+        deletedPapa.left = deleted.left || deleted.right || undefined;
+      } else if (deletedPapa.right === deleted) {
+        deletedPapa.right = deleted.left || deleted.right || undefined;
+      }
+      // 被删节点两个子树
+    } else {
+    }
+    return this.data;
+  }
 }
+
+const BST = new BinarySearchTree(obj);
+console.log(BST.find(14));
+console.log(BST.findWithoutRe(14));
+console.log(BST.findMin());
+console.log(BST.findMinWithoutRe());
+console.log(BST.findMax());
+console.log(BST.findMaxWithoutRe());
+console.log(BST.insert(5));
+console.log(BST.insert(20));
