a856813b365140397b760a8576531933ab6651c1
xiaoj <xiaoj@tencent.com>
2021-09-14 09:00:38 +0800

    zuihoupaidui

--- DirectedGraph/weightedAdjacencyList.ts
+++ DirectedGraph/weightedAdjacencyList.ts
@@ -8,51 +8,69 @@ interface adList {
 }
 
 const adjacencyList: adList[] = [
-  { vertex: 0 },
+  { vertex: 0, weights: 0 },
   {
     vertex: 1,
+    weights: 0,
     next: {
       vertex: 2,
-      next: { vertex: 4 },
+      weights: 2,
+      next: { vertex: 4, weights: 1 },
     },
   },
   {
     vertex: 2,
+    weights: 0,
     next: {
       vertex: 0,
-      next: { vertex: 5, next: { vertex: 4 } },
+      weights: 1,
+      next: { vertex: 5, weights: 10, next: { vertex: 4, weights: 3 } },
     },
   },
   {
     vertex: 3,
+    weights: 0,
     next: {
       vertex: 1,
-      next: { vertex: 6 },
+      weights: 4,
+      next: { vertex: 6, weights: 5 },
     },
   },
   {
     vertex: 4,
+    weights: 0,
     next: {
       vertex: 3,
-      next: { vertex: 5, next: { vertex: 6, next: { vertex: 7 } } },
+      weights: 2,
+      next: {
+        vertex: 5,
+        weights: 2,
+        next: { vertex: 6, weights: 8, next: { vertex: 7, weights: 4 } },
+      },
     },
   },
   {
     vertex: 5,
+    weights: 0,
     next: {
       vertex: 0,
+      weights: 2,
       next: {
         vertex: 7,
+        weights: 6,
       },
     },
   },
   {
     vertex: 6,
+    weights: 0,
   },
   {
     vertex: 7,
+    weights: 0,
     next: {
       vertex: 6,
+      weights: 1,
     },
   },
 ];
@@ -95,6 +113,32 @@ class Graph {
         return adjacentVertexes;
       }
     }
+  }
+  // 获取带权重的（出度）邻接点
+  getAdjacentVertexWithWeight(v: number) {
+    for (let i = 0; i < this.adjacencyList.length; i++) {
+      if (v === this.adjacencyList[i].vertex) {
+        const adjacentVertexes: { vertex: number; weights: number }[] = [];
+        let next = this.adjacencyList[i].next;
+        while (next) {
+          adjacentVertexes.push({ vertex: next.vertex, weights: next.weights });
+          next = next.next;
+        }
+        return adjacentVertexes;
+      }
+    }
+  }
+  // 寻找最小值
+  findMinIndex(array: number[], set: Set<number>) {
+    let min = Infinity;
+    let index = -1;
+    for (let i = 0; i < array.length; i++) {
+      if (!set.has(i) && array[i] < min) {
+        min = array[i];
+        index = i;
+      }
+    }
+    return index;
   }
   // 类似树的先序遍历
   // ！！！有问题
@@ -151,6 +195,40 @@ class Graph {
           dist[adjVertexes[i]] = dist[vertex] + 1;
           path[adjVertexes[i]] = vertex;
           queue.push(adjVertexes[i]);
+        }
+      }
+    }
+    return {
+      dist,
+      path,
+    };
+  }
+  // 有权最短路径
+  Dijkstra(s: number) {
+    const set = new Set<number>();
+    set.add(s);
+    const dist = [];
+    const path = [];
+    const vertexNum = this.getVertexNum();
+    for (let i = 0; i < vertexNum; i++) {
+      dist.push(Infinity);
+      path.push(-1);
+    }
+    dist[s] = 0;
+    const sAdjVtx = this.getAdjacentVertexWithWeight(s);
+    for (let i = 0; i < sAdjVtx.length; i++) {
+      dist[sAdjVtx[i].vertex] = sAdjVtx[i].weights;
+      path[sAdjVtx[i].vertex] = s;
+    }
+    while (1) {
+      const index = this.findMinIndex(dist, set);
+      if (index === -1) break;
+      set.add(index);
+      const sAdjVtx = this.getAdjacentVertexWithWeight(index);
+      for (let i = 0; i < sAdjVtx.length; i++) {
+        if (!set.has(sAdjVtx[i].vertex)) {
+          dist[sAdjVtx[i].vertex] = dist[index] + sAdjVtx[i].weights;
+          path[sAdjVtx[i].vertex] = index;
         }
       }
     }
